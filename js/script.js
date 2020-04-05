daf6dfaed9fb33814f7851bb71bd2cfd9d094d55
Derga <52123281+Derga@users.noreply.github.com>
2020-04-05 15:09:41 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,45 @@
+$.js = function (el) {
+    return $('[data-js=' + el + ']')
+};
+
+function carousel() {
+  $.js('timeline-carousel').slick({
+    infinite: false,
+    arrows: false,
+    dots: true,
+    autoplay: false,
+    speed: 1100,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+  });
+}
+
+carousel();
+
+
+
+
+
+var btn = $('#buttonn');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 800) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:600}, '300');
+});
+
