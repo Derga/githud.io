7207af84a93fbd0781f5f312847f29e505dd34b8
Derga <52123281+Derga@users.noreply.github.com>
2020-04-05 15:10:31 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Sait</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700,900" rel="stylesheet"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css'>
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'>
+
+<script src="js/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<link rel = "stylesheet"  href="css/styles.css">
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- HelloPreload http://hello-site.ru/preloader/ -->
+<style type="text/css">#hellopreloader>p{display:none;}#hellopreloader_preload{display: block;position: fixed;z-index: 99999;top: 0;left: 0;width: 100%;height: 100%;min-width: 1000px;background: rgb(34, 36, 37) url(http://hello-site.ru//main/images/preloads/grid.svg) center center no-repeat;background-size:41px;}</style>
+<div id="hellopreloader"><div id="hellopreloader_preload"></div><p><a href="http://hello-site.ru">Hello-Site.ru. Бесплатный конструктор сайтов.</a></p></div>
+<script type="text/javascript">var hellopreloader = document.getElementById("hellopreloader_preload");function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},800);};</script>
+<!-- HelloPreload http://hello-site.ru/preloader/ -->
+
+<section class = menu>
+<nav>
+<div class="logo">   
+    <a href= "../index.php">
+        <img src = "images/log.png">
+    </a>   
+</div>
+	<ul class = "topmenu">
+    <li><a href="#"  class="submenu-link">Хроника событий</a>
+        <ul class = "submenu">
+			    <li><a href="1914/1914.html">1914 год</a></li>
+			    <li><a href="1915/1915.html">1915 год</a></li>
+			    <li><a href="1916/1916.html">1916 год</a></li>
+			    <li><a href="1917/1917.html">1917 год</a></li>
+			    <li><a href="1918/1918.html">1918 год</a></li>
+	    </ul>
+    </li>
+		<li><a  href= "../articles/article.html">Краткие сведения</a></li>
+		<li><a href="info.html">О проекте</a></li>
+	</ul>
+</nav>
+      
+</section>
+
+
+
+<!-- partial:index.partial.html -->
+<!--Timeline carousel-->
+<section class="timeline-carousel">
+
+    <div class="timeline-carousel__item-wrapper" data-js="timeline-carousel">
+        
+      <!--Timeline item-->
+        <div class="timeline-carousel__item">
+            <div class="timeline-carousel__image">
+                <div class="media-wrapper media-wrapper--overlay" style="background: url('https://www.historyonthenet.com/wp-content/uploads/2016/12/135151-004-0D4D550E.jpg') center center; background-size:cover;"></div>
+            </div>
+            <div class="timeline-carousel__item-inner">
+                <span class="year">1914</span>
+                <span class="month">В июле 1914 г Германия и Австро-венгрия начинают первую мировую войну</span>
+                <p>Германия хотела сначала вывести из строя Францию, чтобы прекратить борьбу на два фронта: Западном и Восточном. Затем нанести удары по России на суше и по Англии на море. Они хотели молниеносной войны.</p>
+              <a href="1914/1914.html" class="read-more">Подробнее о событиях</a>
+            </div>
+        </div> 
+       <!--/Timeline item-->
+       
+        <!--Timeline item-->
+        <div class="timeline-carousel__item">
+            <div class="timeline-carousel__image">
+                <div class="media-wrapper media-wrapper--overlay" style="background: url('http://factsforkids.net/wp-content/uploads/2013/09/13.jpg') center center; background-size:cover;"></div>
+            </div>
+            <div class="timeline-carousel__item-inner">
+            <span class="year">1915</span>
+                <span class="month">Попытка германских войск окружить 10-ю русскую армию в Августовской операции в Восточной Пруссии.</span>
+                <p>Кампания 1915 г. была тяжёлой для русской армии. Сотни тысяч солдат и офицеров были убиты, ранены и взяты в плен. Русская армия оставила Галицию, Буковину, Польшу, часть Прибалтики, Белоруссии.</p>
+                <a href="1915/1915.html" class="read-more">Подробнее о событиях</a>
+            </div>
+        </div>
+       <!--/Timeline item-->
+       
+       <!--Timeline item-->
+        <div class="timeline-carousel__item">
+            <div class="timeline-carousel__image">
+                <div class="media-wrapper media-wrapper--overlay" style="background: url('https://pbs.twimg.com/media/Dp3ODWDXcAAvTXz.jpg:large') center center; background-size:cover;"></div>
+            </div>
+            <div class="timeline-carousel__item-inner">
+                <span class="year">1916</span>
+                <span class="month">Битвы у Вердена и Сомме не дали решающего перевеса ни одной из сторон. Стало ясно, что Германия не сможет выиграть войну</span>
+                <p>Главным событием кампании 1916 г. стало Верденское сражение. Оно считается самым длинным сражением Первой мировой войны  и очень кровопролитным. Поэтому оно получило и другое название: «Верденская мясорубка». </p>
+                <a href="1916/1916.html" class="read-more">Подробнее о событиях</a>
+            </div>
+        </div>
+       <!--/Timeline item-->
+
+       <!--Timeline item-->
+        <div class="timeline-carousel__item">
+            <div class="timeline-carousel__image">
+                <div class="media-wrapper media-wrapper--overlay" style="background: url('https://ichef-1.bbci.co.uk/news/660/media/images/72349000/jpg/_72349652_generals-on-horseback.jpg') center center; background-size:cover;"></div>
+            </div>
+            <div class="timeline-carousel__item-inner">
+            <span class="year">1917</span>
+                <span class="month">Большевистское правительство заключает в декабре перемирие с Германией, и это полностью зачёркивает успехи Антанты</span>
+                <p>В 1917 г. помощь США усилила военное положение стран Антанты, это давало им шанс победы над армиями Центральных держав. Но, к сожалению, Антанта не смогла воспользоваться преимуществом. </p>
+                <a href="1917/1917.html" class="read-more">Подробнее о событиях</a>
+            </div>
+        </div>
+       <!--/Timeline item-->
+       
+       <!--Timeline item-->
+        <div class="timeline-carousel__item">
+            <div class="timeline-carousel__image">
+                <div class="media-wrapper media-wrapper--overlay" style="background: url('https://warfiles.ru/uploads/posts/2017-11/1510415225_a24b6867fcc314ef7edafb9a02a383c47f8f6542.jpg') center center; background-size:cover;"></div>
+            </div>
+            <div class="timeline-carousel__item-inner">
+            <span class="year">1918</span>
+                <span class="month">Амьенская операция</span>
+                <p>Стало понятно, что Центральные державы уже не способны победить и война близится к завершению. Главной задачей германцев теперь стало добиться приемлемых условий заключения мира.</p>
+                <a href="1918/1918.html" class="read-more">Подробнее о событиях</a>
+            </div>
+        </div>
+       <!--/Timeline item-->
+
+
+       <!--/Timeline item-->
+    </div>
+</section>
+<!--Timeline carousel-->
+
+
+
+
+<section class = "maincontent">
+
+<div class = "bloki">
+  
+<ul class="push">
+  <li><a href="#a1">Первая мировая война</a></li>
+  <li><a href="#a2">Страны-участницы</a></li>
+  <li><a href="#a3">Причины войны и интересы сторон</a></li>
+  <li><a href="#a4">Повод к началу войны</a></li>
+
+</ul>
+
+<div id = "verb">
+<h1 class = "interestfacts">Интересные факты</h1>
+<div class = "textscroll" style="overflow: auto; width:85%; height:270px; ">На ближневосточном направлении основным средством передвижения были кони, люди, мулы и верблюды. Существовало два вида верблюжьих формирований. Имперский верблюжий корпус – войсковое соединение Британской империи периода войны. Личный состав передвигался на верблюдах. Корпус был основан в январе 1916 года (четыре батальона). Состав – 4150 человек и 4800 верблюдов. За время ведения военных действий погибло 246 военнослужащих корпуса. «Логистикой» занимался Египетский верблюжий транспортный корпус. Через него прошло 72,5 тыс. верблюдов и около 170 тыс. погонщиков. В 1921 году в Лондоне установлен памятник Имперскому верблюжьему корпусу. На памятнике выбиты имена погибших воинов и названия 20 сражений, в которых он принимал участие. </div>
+</div>
+
+<div><img  class ="imgbok" src="img/img5.png" alt="" width="100%" height="auto"/> </div>
+
+<div> <img class ="imgbok" src = "images/facts1.png" width="100%" height="auto" ></div>
+<div> <img class ="imgbok" src = "images/facts2.png" width="100%" height="auto" ></div>
+<div> <img class ="imgbok" src = "images/facts3.png" width="100%" height="auto" ></div>
+<div> <img class ="imgbok" src = "images/facts4.png" width="100%" height="auto" ></div>
+<div> <img class ="imgbok" src = "images/facts5.png" width="100%" height="auto" ></div>
+<div> <img class ="imgbok" src = "images/facts6.png" width="100%" height="auto" ></div>
+<div> <img class ="imgbok" src = "images/facts7.png" width="100%" height="auto" ></div>
+<div> <img class ="imgbok" src = "images/facts8.png" width="100%" height="auto" ></div>
+
+
+</div>
+
+
+
+<div id = "a1" class = "about">
+<h1>Первая мировая война (1914 - 1918)</h1>
+
+<div class = "first">
+   <p class = "first1">Первая мировая война — один из двух мощнейших и самых страшных вооруженных конфликтов в человеческой истории. В нее были вовлечены несколько стран, каждая из которых серьезно пострадала. Первая мировая война продлилась с 28 июля 1914 по 11 ноября 1918. Факты говорят о том, что за один только первый год войны в военных действиях приняли участие 70 миллионов человек, из них 60 миллионов в Европе, погибло от 9 до 10 миллионов человек. Среди гражданского населения также миллионы жертв. По разным данным от 7 до 12 миллионов мирных граждан погибли в Первой мировой войне, а 55 миллионов человек пострадали.
+ 
+<blockquote class="blockquote-8">
+    <p> Ни один человек на свете не поверил бы, если бы ему сказали в 1914 году, что тогдашние тринадцатилетние дети окажутся участниками войны, что через четыре года она будет в полном разгаре…
+</p>
+    <cite>— Владимир Дмитриевич Набоков, один из лидеров Конституционно-демократической партии (кадетов), отец писателя Владимира Набокова, которому на момент начала войны было 15 лет</cite>
+</blockquote>
+</p>  
+  <p  style = "margin-bottom: 20px;" class=first2> Участниками Первой Мировой войны 1914-1918 годов стали 38 из 59 существовавших на тот момент государств. Эта война навсегда изменила политическую карту мира и ход истории человечества.
+
+   </p>
+</div>
+</div>
+
+
+
+
+
+
+
+
+
+<div id = "a2" class = "about">
+<h1>Страны – участницы Первой мировой войны</h1>
+<img  id="img1" src="img/img1.jpg" alt="" width="600" height="460"/>
+
+
+<div class=first3> 
+Война происходила между двумя группами стран:
+<ul>
+   <li> <span>АНТАНТА: </span>Российская империя, Франция, Великобритания. Союзниками выступали США, Италия, Румыния, Канада, Австралия, новая Зеландия.</li>
+   <li><span> ТРОЙСТВЕННЫЙ СОЮЗ: </span>Германия, Австро-Венгрия, Османская империя. Позже к ним присоединилось Болгарское царство, и коалиция стала именоваться «Четверной союз».</li>
+</ul>
+
+<p>В войне принимали участие следующие крупные страны: Австро-Венгрия (27 июля 1914 - 3 ноября 1918), Германия (1 августа 1914 - 11 ноября 1918), Турция (29 октября 1914 - 30 октября 1918), Болгария (14 октября 1915 - 29 сентября 1918). Страны Антанты и союзники: Россия (1 августа 1914 - 3 марта 1918), Франция (3 августа 1914), Бельгия (3 августа 1914), Великобритания (4 августа 1914), Италия (23 мая 1915), Румыния (27 августа 1916).
+</p>
+<p>Изначально членом "Тройственного союза" также была Италия. Но после начала Первой мировой войны, итальянцы объявили о нейтралитете.
+</p>
+</div>
+</div>
+
+
+
+<div id = "a3" class = "about">
+<h1>   
+Причины войны и интересы сторон
+</h1>
+ <p class=first4> Главной причиной Первой мировой войны явилась борьба за передел мира. Колониальная система сошла на нет, и мировым державам просто негде было брать ресурсы. Кроме того на политической арене появляется молодое сильное государство — Германия, с которым приходилось считаться. На данном фоне возникают противоречия:</p>
+
+<div strstr>
+ <span class = "str">Англия:</span>
+<ul class = "prich">
+<li>вмешательство Германии в экономические дела Восточной и Юго-Западной Африки, сфер влияния Великобритании;</li>
+<li>поддержки буров Германией в войне с Англией;</li>
+<li>сохранение морского и колониального могущества;</li>
+<li>захват у Османской Империи Месопотамии и Палестины, которые были богаты нефтью. В данном вопросе главным конкурентом Великобритании была снова Германия.</li>
+</ul>
+</div>
+
+<div strstr>
+ <span class = "str">Франция:</span>
+<ul class = "prich">
+<li>противоречия в экономической политике;</li>
+<li>желание вернуть в свой состав Эльзас и Лотарингию;</li>
+<li>притязания на Саарский угольный бассейн.</li>
+</ul>
+</div>
+
+<div strstr>
+ <span class = "str">Российская Империя:</span>
+<ul class = "prich">
+<li>контроль за проливами Черного моря (Басфор и Дарданеллы). Россия боялась, что Турция займет лидирующие позиции в Черноморских проливах, особенно когда командующим турецкой армией был назначен немецкий генерал фон Сандерс;</li>
+<li>защита славянских народов на Балканах. Австро-Венгрия с Германией агрессивно действовали на Балканском полуострове, не слишком считаясь с мнением Российской Империи;</li>
+<li>противоречия с Берлином из-за строительства железной дороги Берлин — Багдад;</li>
+<li>присоединить к землям Империи Галицию.</li>
+</ul>
+</div>
+
+
+<div strstr>
+ <span class = "str">Сербия:</span>
+<ul class = "prich">
+<li>получить полную независимость.</li>
+</ul>
+</div>
+
+<div strstr>
+ <span class = "str">Болгария:</span>
+<ul class = "prich">
+<li>закрепить свои позиции на Балканах.</li>
+</ul>
+</div>
+
+<div strstr>
+ <span class = "str">Австро-Венгрия:</span>
+<ul class = "prich">
+<li>усиление влияния на Балканском полуострове;</li>
+<li>оставить Боснию и Герцеговину под своим контролем;</li>
+<li>отделить от России несколько польских губерний.</li>
+</ul>
+</div>
+
+<div strstr>
+ <span class = "str">Германия:</span>
+<ul class = "prich">
+<li>политическое господство на мировой арене;</li>
+<li>получить контроль над колониями европейских государств и Великобритании;</li>
+<li>Противодействие России в защите балканских славян и закрепиться на Балканах;</li>
+<li>лишить Великобританию господства в море;</li>
+<li>отделить от России Польшу, Прибалтику и Украину.</li>
+
+</ul>
+</div>
+
+
+<div strstr>
+ <span class = "str">Османская империя:</span>
+<ul class = "prich">
+<li>вернуть в свой состав территории, утраченные в Балканских войнах;</li>
+<li>сохранение единения нации.</li>
+</ul>
+</div>
+
+
+<div strstr>
+ <span class = "str">Другие причины начала Первой мировой войны:</span>
+<ul class = "prich">
+<li>Еще одним мотивом был русского-польский вопрос. Россия активно проводила политику русификации поляков, а Польша в свою очередь стремилась получить независимость от Российской Империи.</li>
+<li>Урегулирование революционных движений в некоторых странах. Особенно данный вопрос был актуален для Российской Империи, которая переживала сложное внутриполитическое время.</li>
+<li>Кроме всего прочего: научно-технический прогресс не стоял на месте и изобреталось новое оружие, которое было использовано для демонстрации мощи крупных и новых государств. Во время Первой мировой войны было впервые использовано химическое оружие, ядовитые газы, танки, огнеметы. Кроме того появились первые противогазы.</li>
+</ul>
+</div>
+ 
+
+</div>
+
+
+
+
+
+
+<div id = "a4" class = "about">
+<h1>Повод к началу войны</h1>
+<p class=first4> "Толчком" к началу Первой мировой войны послужили события в Сараево (Босния и Герцеговина). 28 июня 1914 года член организации «Черная рука» движения «Молодая Босния» Гаврило Принцип убил эрцгерцога Франса Фердинанда. Фердинанд был наследником австро-венгерского престола, поэтому резонанс у убийства был громадный. Это был повод Австро-Венгрии напасть на Сербию.</p>
+<img class ="dead" id="img" src="img/img2.jpg" alt="Убийство эрцгерцога Франса Фердинанда" />
+<p class=first2> Здесь очень важно поведение Англии, поскольку Австро-Венгрия самостоятельно не могла начать войну, ведь это практически гарантировало войну во всей Европе. Англичане на уровне посольства убеждали Николая 2, что Россия в случае агрессии не должна оставлять Сербию без помощи. Но тут же вся английская пресса писала о том, что сербы варвары и Австро-Венгрия не должна оставлять безнаказанным убийство эрцгерцога. То есть, Англия сделала все, чтобы Австро-Венгрия, Германия и Россия не уклонялись от войны.</p>
+
+</div>
+
+
+
+</section >
+
+
+
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
+<script src='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.js'></script>
+<script  src="js/script.js"></script>
+
+</body>
+</html>
+
+
---
+++ script.js
@@ -0,0 +1,25 @@
+$.js = function (el) {
+    return $('[data-js=' + el + ']')
+};
+
+function carousel() {
+  $.js('timeline-carousel').slick({
+    infinite: false,
+    arrows: false,
+    dots: true,
+    autoplay: false,
+    speed: 1100,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }]
+  });
+}
+
+carousel();
